98dd8ba47d1dada70676a31585af6213257020b2
Zacas6 <szacariasvasquez@gmail.com>
2025-09-30 22:14:44 -0600

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,15 @@ btnSignIn.addEventListener("click",()=> {
 btnSignUp.addEventListener("click",()=> {
     container.classList.add("toggle");
 });
+const togglePassword = document.getElementById("togglePassword");
+const password = document.getElementById("password");
+
+togglePassword.addEventListener("click", () => {
+  if (password.type === "password") {
+    password.type = "text";
+    togglePassword.setAttribute("name", "eye-off-outline"); 
+  } else {
+    password.type = "password";
+    togglePassword.setAttribute("name", "eye-outline");
+  }
+});
